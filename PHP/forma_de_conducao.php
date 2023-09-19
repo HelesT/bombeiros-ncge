ae4210d6e00086f44f474848f2bad2bcb0b77f46
HelesT <nyltondudu29@gmail.com>
2023-09-19 11:46:47 -0300

    a

--- PHP/forma_de_conducao.php
+++ PHP/forma_de_conducao.php
@@ -4,15 +4,10 @@ include("conecta.php");
 $Conducao = $_POST['conducao'];
 $FormaConducao = $_POST['FormaConducao'];
 
-/* fazer banco de dados*/
-/*$stmt = $pdo->prepare("INSERT INTO paciente
-(nome_hospital_paciente, data_paciente, sexo_paciente,
-nome_paciente, idade_paciente, rg_cpf_paciente,
-fone_paciente, acompanhante_paciente, idade_acompanhante_paciente,
-local_ocorrencia_paciente, n_usb_paciente, n_ocorr_paciente,
-desp_paciente, h_ch_paciente, km_final_paciente,
-cod_sia_sus_paciente, cod_ur_paciente, cod_ps_paciente) VALUES 
-(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");*/
+/*fazer banco de dados*/
+$stmt = $pdo->prepare("INSERT INTO forma_conducao
+(conducao, forma_conducao) VALUES 
+(?, ?)");*/
 $stmt->execute([
     $$Conducao, $FormaConducao,
 ]);
