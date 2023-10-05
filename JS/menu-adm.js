30299e0e0b47a5392587bf9c89692a9da3eb8f25
HelesT <nyltondudu29@gmail.com>
2023-10-04 22:28:33 -0300

    AE KCT

--- JS/menu-adm.js
+++ JS/menu-adm.js
@@ -17,7 +17,11 @@ function AdicionarGeral(){
            acesso: AcessoGeral
         },
         dataType: 'json'
-    })
+    }).done(function(){
+        ChamarRegistro();
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("Erro na requisição AJAX: " + errorThrown);
+    });
 };
 
 function EditarGeral(){
@@ -38,7 +42,11 @@ function EditarGeral(){
            acesso: AcessoGeral
         },
         dataType: 'json'
-    })
+    }).done(function(){
+        ChamarRegistro();
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("Erro na requisição AJAX: " + errorThrown);
+    });
 };
 
 function ExcluirGeral(){
@@ -59,5 +67,29 @@ function ExcluirGeral(){
            acesso: AcessoGeral
         },
         dataType: 'json'
-    })
+    }).done(function(){
+        ChamarRegistro();
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("Erro na requisição AJAX: " + errorThrown);
+    });
 }
+
+function ChamarRegistro(){
+    
+
+    $.ajax({
+        url: 'PHP/adm-chamar-registro.php',
+        method: 'GET',
+        dataType: 'json'
+    }).done(function(result){
+        $('.exibir').empty();
+
+        for (var i = 0; i < result.length; i++) {
+            $('.exibir').prepend('<div id="' + result[i].cod_cadastro + '" class="linha-tabela justify"><div class="space-evenly" style="width: 900px;"><input class="input-text-usuario" value="' + result[i].nome_cadastro + '" readonly><div style="width: 1px; height: 52px; background-color: black;"></div><input class="input-text-usuario" value="' + result[i].senha_cadastro + '" readonly><div style="width: 1px; height: 52px; background-color: black;"></div><input class="input-text-usuario" value="' + result[i].cod_cadastro + '" readonly><div style="width: 1px; height: 52px; background-color: black;"></div></div><div class="space-evenly justify" style="width: 250px ;height: 100%; float: right;"><button style="width: 20px; height: 20px; background-color: black;"></button><button style="width: 20px; height: 20px; background-color: black;"></button><button style="width: 20px; height: 20px; background-color: black;"></button></div></div>');
+        }
+        
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        console.log("Erro na requisição AJAX: " + errorThrown);
+    });
+}
+
