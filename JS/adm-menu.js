3492b17d58572d740c0ce1cf101831b236ba5d12
HelesT <nyltondudu29@gmail.com>
2023-11-27 19:13:45 -0300

    commit

--- JS/adm-menu.js
+++ JS/adm-menu.js
@@ -2,6 +2,10 @@
 //CADASTRO PACIENTES
 
 //CADASTRO BOMBEIROS-----------------------------------------------------------------//
+$(document).ready(function() {
+    ChamarRegistro();
+});
+
 function AdicionarGeral(){
 
     var NomeGeral = $("#nome_geral").val();
@@ -96,7 +100,7 @@ function ChamarRegistro(){
         $('.exibir').empty();
 
         for (var i = 0; i < result.length; i++) {
-            $('.exibir').prepend('<div id="' + result[i].cod_cadastro + '" class="linha-tabela justify div-pai"><div class="space-evenly" style="width: 900px;"><input class="input-text-usuario" value="' + result[i].nome_cadastro + '" readonly><div style="width: 1px; height: 52px; background-color: black;"></div><input class="input-text-usuario" value="' + result[i].senha_cadastro + '" readonly><div style="width: 1px; height: 52px; background-color: black;"></div><input class="input-text-usuario" value="' + result[i].cod_cadastro + '" readonly><div style="width: 1px; height: 52px; background-color: black;"></div></div><div class="space-evenly justify" style="width: 180px ;height: 100%; float: right;"><button class="centro" style="border:1px solid black; width: 20px; height: 20px;  background-color: rgb(230, 154, 12); cursor: pointer;" onclick="EditarGeral()"><img src="IMAGENS/edit.png" width="15px"></button><button class="centro" style="border:1px solid black; width: 20px; height: 20px;  background-color: rgb(199, 114, 114); cursor: pointer;" onclick="ExcluirGeral(' + result[i].cod_cadastro + ')"><img src="IMAGENS/lixo.png" width="13px"></button></div></div>');
+            $('.exibir').prepend('<div id="' + result[i].cod_cadastro + '" class="linha-tabela justify div-pai"><div class="space-evenly" style="width: 900px;"><input class="input-text-usuario" value="' + result[i].nome_cadastro + '" id="nomeEspecifico' + result[i].cod_cadastro + '" readonly><div style="width: 1px; height: 52px; background-color: black;"></div><input class="input-text-usuario" value="' + result[i].senha_cadastro + '" id="senhaEspecifico' + result[i].cod_cadastro + '" readonly><div style="width: 1px; height: 52px; background-color: black;"></div><input class="input-text-usuario" value="' + result[i].cod_cadastro + '" id="codigoEspecifico' + result[i].cod_cadastro + '" readonly><div style="width: 1px; height: 52px; background-color: black;"></div></div><div class="space-evenly justify" style="width: 180px ;height: 100%; float: right;"><button class="centro" style="border:1px solid black; width: 20px; height: 20px;  background-color: rgb(230, 154, 12); cursor: pointer;" onclick="editarNomeEspecificdo(' + result[i].cod_cadastro + ')"><img src="IMAGENS/edit.png" width="15px"></button><button class="centro" style="border:1px solid black; width: 20px; height: 20px;  background-color: rgb(199, 114, 114); cursor: pointer;" onclick="ExcluirGeral(' + result[i].cod_cadastro + ')"><img src="IMAGENS/lixo.png" width="13px"></button></div></div>');
         }
         
     }).fail(function(errorThrown) {
@@ -104,11 +108,6 @@ function ChamarRegistro(){
         ChamarRegistro();
     });
 }
-
-$(document).ready(function() {
-    ChamarRegistro();
-});
-
 
 var menuIcons = document.getElementsByClassName("menu-icon");//TRAZ BARRA LATERAL PARA A TELA E A RETIRA
 var barraLateral = document.querySelector(".barra-Lateral");
@@ -159,18 +158,15 @@ function executarSelect(){
             $('.exibir').empty();
         
             for (var i = 0; i < result.length; i++) {
-                $('.exibir').prepend('<div id="' + result[i].cod_cadastro + '" class="linha-tabela justify div-pai"><div class="space-evenly" style="width: 900px;"><input class="input-text-usuario" value="' + result[i].nome_cadastro + '" readonly><div style="width: 1px; height: 52px; background-color: black;"></div><input class="input-text-usuario" value="' + result[i].senha_cadastro + '" readonly><div style="width: 1px; height: 52px; background-color: black;"></div><input class="input-text-usuario" value="' + result[i].cod_cadastro + '" readonly><div style="width: 1px; height: 52px; background-color: black;"></div></div><div class="space-evenly justify" style="width: 180px ;height: 100%; float: right;"><button style="width: 20px; height: 20px; background-color: black;"></button><button style="width: 20px; height: 20px; background-color: black;"></button><button style="width: 20px; height: 20px; background-color: black;"></button></div></div>');
+                $('.exibir').prepend('<div id="' + result[i].cod_cadastro + '" class="linha-tabela justify div-pai"><div class="space-evenly" style="width: 900px;"><input class="input-text-usuario" value="' + result[i].nome_cadastro + '" readonly><div style="width: 1px; height: 52px; background-color: black;"></div><input class="input-text-usuario" value="' + result[i].senha_cadastro + '" readonly><div style="width: 1px; height: 52px; background-color: black;"></div><input class="input-text-usuario" value="' + result[i].cod_cadastro + '" readonly><div style="width: 1px; height: 52px; background-color: black;"></div></div><div class="space-evenly justify" style="width: 180px ;height: 100%; float: right;"><button class="centro" style="border:1px solid black; width: 20px; height: 20px;  background-color: rgb(230, 154, 12); cursor: pointer;" onclick="EditarGeral()"><img src="IMAGENS/edit.png" width="15px"></button><button class="centro" style="border:1px solid black; width: 20px; height: 20px;  background-color: rgb(199, 114, 114); cursor: pointer;" onclick="ExcluirGeral(' + result[i].cod_cadastro + ')"><img src="IMAGENS/lixo.png" width="13px"></button></div></div>');
             }
                 
         }).fail(function(errorThrown) {
             console.log(errorThrown);
-            OpcaoNomeSelect();
         }); 
 
-    }else if (OpcaoSelecionada === "opcao_data") {
-        console.log("batata2")
-    }else{
-        ChamarRegistro();
+    }else if(OpcaoSelecionada === "opcao_nada"){
+        ChamarRegistro()
     }
 }
 
@@ -181,6 +177,54 @@ function MudarOrdemRegistro() {
     $('.exibir').html(divsArray); // Atualiza o conteúdo do elemento .exibir
 }
 
+function editarNomeEspecificdo(id){
+    var nomeEspId = document.getElementById("nomeEspecifico" + id)
+    var senhaEspId = document.getElementById("senhaEspecifico" + id)
+    var codeEspId = document.getElementById("codigoEspecifico" + id)
+    
+    if (nomeEspId.readOnly === true) {
+       
+        nomeEspId.classList.toggle("input-text-usuario-active");
+        nomeEspId.classList.remove("input-text-usuario");
+        nomeEspId.removeAttribute("readonly");
+    
+        senhaEspId.classList.toggle("input-text-usuario-active");
+        senhaEspId.classList.remove("input-text-usuario");
+        senhaEspId.removeAttribute("readonly");
+    } else {
+        nomeEspId.classList.toggle("input-text-usuario");
+        nomeEspId.classList.remove("input-text-usuario-active");
+        nomeEspId.readOnly = true;
+        
+        senhaEspId.classList.toggle("input-text-usuario");
+        senhaEspId.classList.remove("input-text-usuario-active");
+        senhaEspId.readOnly = true;
+
+        $.ajax({
+            url: 'PHP/adm-editar-especifico.php',
+            method: 'POST',
+            data: {
+               acesso: codeEspId.value,
+               nome: nomeEspId.value,
+               senha: senhaEspId.value
+            },
+            dataType: 'json'
+        }).done(function(response){
+            if(response.success){
+                alert("Edição Realizada!")
+            }
+
+        }).fail(function(errorThrown) {
+            console.log(errorThrown);
+
+        });
+
+    }
+    
+    
+
+    
+}
 //CADASTRO PACIENTES-----------------------------------------------------------------//
 
 
--- PHP/adm-ordenar-paciente-data.php
+++ PHP/adm-ordenar-paciente-data.php
@@ -3,7 +3,7 @@ include("conecta.php");
 
 $stmt = $pdo->prepare('SELECT paciente.cpf_paciente, paciente.nome_paciente, COALESCE(tabela_paciente.data_paciente, "Sem registro") as data_paciente
                       FROM paciente
-                      LEFT JOIN tabela_paciente ON paciente.cpf_paciente = tabela_paciente.cpf_paciente');
+                      LEFT JOIN tabela_paciente ON paciente.cpf_paciente = tabela_paciente.cpf_paciente ORDER BY tabela_paciente.data_paciente DESC');
 $stmt->execute();
 
 $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
